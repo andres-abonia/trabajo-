c7e56a87513e151243e7631b726d74a0c6334e88
andres-abonia <andres@andres-vivobook-asuslaptop.www.nexxtwifi.com>
2021-08-27 13:16:27 -0500

    terminamos con los cambios la master

---
+++ js/lightbox.js
@@ -0,0 +1,29 @@
+const imagenes = document.querySelectorAll('.img-galeria')
+const imageneslight = document.querySelector('.agregar-imagen')
+const contenedorlight = document.querySelector('.imagen-light')
+const hamburguer1 = document.querySelector('.hamburger'); 
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerimagen(imagen.getAttribute('src'))
+    })
+})
+
+
+contenedorlight.dispatchEvent('click',(e)=>{
+    if(e.target !== imageneslight){
+        contenedorlight.classList.toggle('show')
+    imageneslight.classList.toggle('showimage')
+    hamburguer.style.opacity = '1'
+
+    }
+})
+
+
+const aparecerimagen =(imagen)=>{
+    imageneslight.src = imagen;
+    contenedorlight.classList.toggle('show')
+    imageneslight.classList.toggle('showimage')
+    hamburguer.style.opacity = '0'
+}
